7a7ce8749fa1d6482a34b32a0e37d7cda402940a
Damien Roche <damienroche@free.fr>
2017-03-22 23:52:10 +0100

    split ruby in specific file

--- rails.sh
+++ rails.sh
@@ -1,10 +1,5 @@
-# install ruby with rbenv
-brew install rbenv ruby-build && source ~/.zshrc
-rbenv install 2.3.3
-rbenv global 2.3.3
-
 # Before install Rails be sure ruby -v is 2.3.3
-# if not add rbenv instructions to your ~/.bash_profile ou ~/.zshrc
+# if not add "rbenv init" instructions to your ~/.bash_profile ou ~/.zshrc
 
 # install Rails
 gem install bundler pry hub
---
+++ ruby.sh
@@ -0,0 +1,4 @@
+# install ruby with rbenv
+brew install rbenv ruby-build && source ~/.zshrc
+rbenv install 2.3.3
+rbenv global 2.3.3
