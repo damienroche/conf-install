c5c42c8c8ae07f83893bd40e00a83b76be351693
Damien Roche <damienroche@free.fr>
2017-03-22 23:58:24 +0100

    add git.sh

---
+++ git.sh
@@ -0,0 +1,10 @@
+echo "Type in your first and last name (no accent or special characters - e.g. 'ç'): "
+read full_name
+
+echo "Type in your email address (the one used for your GitHub account): "
+read email
+
+git config --global user.email $email
+git config --global user.name $full_name
+git config --global core.editor "vim" && \
+git config --global color.ui true
