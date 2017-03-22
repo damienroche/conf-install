2d3ae5767a4beb2160cebe5fc274bfe44b923f98
Damien Roche <damienroche@free.fr>
2017-03-22 19:03:45 +0100

    init zsh.sh script

---
+++ zsh.sh
@@ -0,0 +1,12 @@
+# Zsh Theme
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+	CURRENT_DIR=`pwd`
+	THEME_DIR="$HOME/.oh-my-zsh/themes"
+	cd "$THEME_DIR"
+	if [ ! -f "$THEME_DIR/damien.zsh-theme" ]; then
+		echo "-----> Backup Zsh Theme..."
+		curl https://raw.githubusercontent.com/damienroche/conf-dotfiles/master/damien.zsh-theme > damien.zsh-theme
+	fi
+	source ~/.zshrc
+	cd "$CURRENT_DIR"
+fi
