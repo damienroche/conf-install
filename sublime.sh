080ca255f57e6009388fc271864000e38f18d17e
Damien Roche <damienroche@free.fr>
2017-03-23 00:35:49 +0100

    sublime script

---
+++ sublime.sh
@@ -0,0 +1,4 @@
+# install package control
+$PACK_PATH = $HOME/Library/Application Support/Sublime Text 3/Installed Packages
+$USER_PATH = $HOME/Library/Application Support/Sublime Text 3/Packages/User
+wget https://packagecontrol.io/Package%20Control.sublime-package >> $PACK_PATH/Package Control.sublime.package
