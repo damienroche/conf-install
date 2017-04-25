9594cbd69002b816b13f63c78b194f1680f13a4c
Damien Roche <droche@student.42.fr>
2017-04-25 17:34:32 +0200

    add smart git log command with git lg alias

--- git.sh
+++ git.sh
@@ -8,3 +8,4 @@ git config --global user.email $email
 git config --global user.name $full_name
 git config --global core.editor "vim" && \
 git config --global color.ui true
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
